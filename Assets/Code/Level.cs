b60e609fea6438f22e290325c9ea4d11c1b175a3
michael <michaelr@thoopid.com>
2018-05-15 20:15:22 +0200

    fixed double gap!

--- Assets/Code/Level.cs
+++ Assets/Code/Level.cs
@@ -9,19 +9,19 @@ public class Level : MonoBehaviour {
     [SerializeField]
     SpriteRenderer spriteRenderer;
 
-    [Header("Enemies"), SerializeField]
+    [Header ("Enemies"), SerializeField]
     GameObject enemyPrefab;
 
     [SerializeField]
     Transform enemyParent;
 
-    [Header("Gaps"), SerializeField]
+    [Header ("Gaps"), SerializeField]
     GameObject gapPrefab;
 
     [SerializeField]
     Transform gapParent;
-    
-    [Header("Collectables"), SerializeField]
+
+    [Header ("Collectables"), SerializeField]
     GameObject collectablePrefab;
 
     [SerializeField]
@@ -49,36 +49,36 @@ public class Level : MonoBehaviour {
 
     public void Initialize () {
         // set random color
-        spriteRenderer.color = GetLevelColor();
+        spriteRenderer.color = GetLevelColor ();
 
-        CreateGaps();
-        CreateCollectables();
-        CreateEnemies();
+        CreateGaps ();
+        CreateCollectables ();
+        CreateEnemies ();
 
         // set random rotation
         var euler = transform.eulerAngles;
-        euler.z = Random.Range(0f, 360f);
+        euler.z = Random.Range (0f, 360f);
         transform.eulerAngles = euler;
     }
 
     public void ShowLevel () {
-        gameObject.SetActive(true);
+        gameObject.SetActive (true);
         //spriteRenderer.color = new Color(spriteRenderer.color.r, spriteRenderer.color.g, spriteRenderer.color.b, 1f);
-        gameObject.transform.SetParent(levelManager.transform);
+        gameObject.transform.SetParent (levelManager.transform);
         IsActive = true;
     }
 
     public void HideLevel () {
-        gameObject.SetActive(false);
+        gameObject.SetActive (false);
         // spriteRenderer.color = new Color(spriteRenderer.color.r, spriteRenderer.color.g, spriteRenderer.color.b, 0.1f);
-        gameObject.transform.SetParent(levelManager.LevelPool);
+        gameObject.transform.SetParent (levelManager.LevelPool);
         IsActive = false;
     }
 
     Color GetLevelColor () {
-        var colors = new List<Color>(levelManager.LevelColors.Length);
-        colors.AddRange(levelManager.LevelColors);
-        return colors[Random.Range(0, levelManager.LevelColors.Length)];
+        var colors = new List<Color> (levelManager.LevelColors.Length);
+        colors.AddRange (levelManager.LevelColors);
+        return colors[Random.Range (0, levelManager.LevelColors.Length)];
     }
 
     void CreateGaps () {
@@ -88,25 +88,24 @@ public class Level : MonoBehaviour {
         }
 
         // get random amount of gaps to spawn
-        var amount = Random.Range(2, 5);
+        var amount = Random.Range (2, 5);
 
-        var unusedRotations = GetGapRoationList();
+        var unusedRotations = GetGapRoationList ();
 
         for (var i = 0; i < amount; i++) {
-            var newRotation = GetRandomGapRoation(unusedRotations);
-
-            var go = Instantiate(gapPrefab);
-            go.transform.SetParent(gapParent);
+            var newRotation = GetRandomGapRoation (unusedRotations);
+            var go = Instantiate (gapPrefab);
+            go.transform.SetParent (gapParent);
             go.transform.localScale = transform.localScale;
             go.name = "Gap " + (i + 1);
 
             // set random rotation
             var eulerVector = go.transform.eulerAngles;
-            eulerVector.z = Math.Abs(newRotation);
-            go.transform.localRotation = Quaternion.Euler(eulerVector);
+            eulerVector.z = Math.Abs (newRotation);
+            go.transform.localRotation = Quaternion.Euler (eulerVector);
         }
     }
-    
+
     void CreateCollectables () {
         // don't create collectables if it's the first level
         if (levelManager.Levels.Count == 0) {
@@ -114,26 +113,26 @@ public class Level : MonoBehaviour {
         }
 
         // get random amount of collectables to spawn
-        var amount = Random.Range(3, 10);
+        var amount = Random.Range (3, 10);
 
         for (var i = 0; i < amount; i++) {
-            var go = Instantiate(collectablePrefab).GetComponent<Collectable>();
-            go.transform.SetParent(collectableParent);
+            var go = Instantiate (collectablePrefab).GetComponent<Collectable> ();
+            go.transform.SetParent (collectableParent);
             go.transform.localScale = transform.localScale;
             go.name = "Collectable " + (i + 1);
 
             // set random rotation
             var euler = go.transform.eulerAngles;
-            euler.z = Random.Range(0f, 360f);
+            euler.z = Random.Range (0f, 360f);
             go.transform.eulerAngles = euler;
 
             // check overlapping
-            var collisions = Physics2D.OverlapCircleAll(go.CollectableTransform.position, go.Collider.radius);
+            var collisions = Physics2D.OverlapCircleAll (go.CollectableTransform.position, go.Collider.radius);
             var loopSafety = 0;
             while (collisions.Length > 1 && loopSafety < 20) {
                 euler.z += 20f;
                 go.transform.eulerAngles = euler;
-                collisions = Physics2D.OverlapCircleAll(go.CollectableTransform.position, go.Collider.radius);
+                collisions = Physics2D.OverlapCircleAll (go.CollectableTransform.position, go.Collider.radius);
                 loopSafety++;
             }
         }
@@ -146,44 +145,44 @@ public class Level : MonoBehaviour {
         }
 
         // get random amount of enemies to spawn
-        var amount = Random.Range(0, levelManager.Levels.Count - 9);
+        var amount = Random.Range (0, levelManager.Levels.Count - 9);
 
         for (var i = 0; i < amount; i++) {
-            var go = Instantiate(enemyPrefab).GetComponent<Enemy>();
-            go.transform.SetParent(enemyParent);
+            var go = Instantiate (enemyPrefab).GetComponent<Enemy> ();
+            go.transform.SetParent (enemyParent);
             go.transform.localScale = transform.localScale;
             go.name = "Enemy " + (i + 1);
 
             // set random rotation
             var euler = go.transform.eulerAngles;
-            euler.z = Random.Range(0f, 360f);
+            euler.z = Random.Range (0f, 360f);
             go.transform.eulerAngles = euler;
 
             // check overlapping
-            var collisions = Physics2D.OverlapCircleAll(go.EnemyTransform.position, go.Collider.radius);
+            var collisions = Physics2D.OverlapCircleAll (go.EnemyTransform.position, go.Collider.radius);
             var loopSafety = 0;
             while (collisions.Length > 1 && loopSafety < 20) {
                 euler.z += 20f;
                 go.transform.eulerAngles = euler;
-                collisions = Physics2D.OverlapCircleAll(go.EnemyTransform.position, go.Collider.radius);
+                collisions = Physics2D.OverlapCircleAll (go.EnemyTransform.position, go.Collider.radius);
                 loopSafety++;
             }
         }
     }
 
     List<int> GetGapRoationList () {
-        var rotations = new List<int>(8);
+        var rotations = new List<int> (8);
         for (var i = 0; i < 8; i++) {
-            rotations.Add(i);
+            rotations.Add (i);
         }
         return rotations;
     }
 
     int GetRandomGapRoation (List<int> rotations) {
-        var multiple = Random.Range(rotations[0], rotations.Count);
+        var multiple = rotations[Random.Range (0, rotations.Count)];
         var angle = 45;
         var rotation = multiple * angle;
-        rotations.Remove(multiple);
+        rotations.Remove (multiple);
         return rotation;
     }
 
